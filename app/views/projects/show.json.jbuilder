2d6b4a842b9b8e7d3db5bea26a2e5f1b5ed384f1
MiroBabic <babic003@gmail.com>
2016-01-24 17:58:00 +0000

    first

---
+++ app/views/projects/show.json.jbuilder
@@ -0,0 +1,1 @@
+json.extract! @project, :id, :name, :user_id, :about, :amount, :created_at, :updated_at
---
+++ db/migrate/20160123211221_add_field_to_project.rb
@@ -0,0 +1,5 @@
+class AddFieldToProject < ActiveRecord::Migration
+  def change
+  	add_reference :projects, :category, index: true
+  end
+end
