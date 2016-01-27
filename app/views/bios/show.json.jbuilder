fbfbbf11c913666b2bf791e3e7ae7c6b0d87e377
MiroBabic <babic003@gmail.com>
2016-01-27 21:30:34 +0000

    added bio

---
+++ app/views/bios/show.json.jbuilder
@@ -0,0 +1,1 @@
+json.extract! @bio, :id, :name, :street, :city, :zip, :phone, :contactemail, :picture, :fbprofile, :gplusprofile, :twitterprofile, :personalweb, :aboutme, :user_id, :created_at, :updated_at
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,27 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160123211221) do
+ActiveRecord::Schema.define(version: 20160127211840) do
+
+  create_table "bios", force: :cascade do |t|
+    t.string   "name"
+    t.string   "street"
+    t.string   "city"
+    t.integer  "zip"
+    t.string   "phone"
+    t.string   "contactemail"
+    t.binary   "picture"
+    t.string   "fbprofile"
+    t.string   "gplusprofile"
+    t.string   "twitterprofile"
+    t.string   "personalweb"
+    t.string   "aboutme"
+    t.integer  "user_id"
+    t.datetime "created_at",     null: false
+    t.datetime "updated_at",     null: false
+  end
+
+  add_index "bios", ["user_id"], name: "index_bios_on_user_id"
 
   create_table "categories", force: :cascade do |t|
     t.string   "name"
