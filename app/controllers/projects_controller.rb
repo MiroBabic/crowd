90dfd1ace9a0ebd4f7f4a9d943fe0361caa71425
MiroBabic <babic003@gmail.com>
2016-02-07 09:29:35 +0000

    new project created

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -25,8 +25,10 @@ class ProjectsController < ApplicationController
   # POST /projects
   # POST /projects.json
   def create
-    @project = Project.new(project_params)
 
+    @project = Project.new(project_params)
+    @project.user_id = current_user.id
+    
     respond_to do |format|
       if @project.save
         format.html { redirect_to @project, notice: 'Project was successfully created.' }
@@ -70,6 +72,6 @@ class ProjectsController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def project_params
-      params.require(:project).permit(:name, :user_id, :about, :amount)
+      params.require(:project).permit(:name, :user_id, :about, :amount, :category_id, :duration)
     end
 end
