22bd05b5d270ff31a9161c9e407887da632d914f
MiroBabic <babic003@gmail.com>
2016-02-29 22:20:47 +0000

    404

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,6 +3,11 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
   
+   rescue_from ActiveRecord::RecordNotFound, :with => :render_404
+  # Render 404 page when record not found
+    def render_404      
+       render :file => "public/404.html", :status => 404
+    end
   
 
 end
