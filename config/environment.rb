be04baa0b54569130aebc61ff2d84c034c8ec1c2
MiroBabic <babic003@gmail.com>
2016-03-12 12:45:37 +0000

    added mailing and design changes of registrations views

--- config/environment.rb
+++ config/environment.rb
@@ -3,5 +3,10 @@ require File.expand_path('../application', __FILE__)
 
 #Rails.logger = Logger.new(STDOUT)
 
+#load env with username and pass for email
+app_env_vars = File.join(Rails.root, 'config', 'initializers', 'app_env_vars.rb')
+load(app_env_vars) if File.exists?(app_env_vars)
+###
+
 # Initialize the Rails application.
 Rails.application.initialize!
