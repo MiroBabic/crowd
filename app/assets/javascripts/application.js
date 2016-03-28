3720f21dd011a0fdc15af7cacbbef2fd004395e0
MiroBabic <babic003@gmail.com>
2016-03-28 22:39:48 +0100

    translations

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -13,7 +13,6 @@
 //= require jquery
 //= require jquery_ujs
 //= require jquery.turbolinks
-//= require jquery-fileupload
 //= require modernizr
 //= require bootstrap-sprockets
 //= require app.js
