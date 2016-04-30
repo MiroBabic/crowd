da00e25f03c4be4d880b0b9ccfd53078aec78372
MiroBabic <babic003@gmail.com>
2016-04-30 19:01:07 +0100

    minor changes

--- app/models/payment.rb
+++ app/models/payment.rb
@@ -3,5 +3,8 @@ class Payment < ActiveRecord::Base
   belongs_to :project
   belongs_to :reward
 
+  include ActiveModel::Validations
+  
+     validates_numericality_of :amount, :greater_than => 0
   
 end
